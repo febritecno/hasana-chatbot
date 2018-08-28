b92fb771a9d9061b3eccacadf002c440b60f7f17
Glitch (hasana) <none>
2018-08-28 12:57:22 +0000

    🌒🕍 Updated with Glitch

--- scrape.js
+++ scrape.js
@@ -67,8 +67,9 @@ var free = (cape) => {
 var diskon = (cape) => {
   
   /** cara menselector selector pertama/seluruh selector elemen yang punya identisas sama bukan paling atas pada target ya !! 
-  
-  contoh disini adalah class .coupon yaitu identisan yg sama pada seluruh komponen yang mau kita ambil. misal
+  contoh disini adalah class .coupon yaitu identitas yg sama pada seluruh komponen yang mau kita ambil. misal
+   
+  mau ambil data link html ketiga ini
   
   <div class='satu post'>
       <a href= 'satu.html'></a>
@@ -80,41 +81,14 @@ var diskon = (cape) => {
       <a href= 'tiga.html'></a>
   <div>
   
-  select saja .post jadi xray('url', '.post', [{}])
-  
+  select saja .post jadi xray('url', '.post', [{}]) karna sama
   
   tapi yang sama tiap konten pada area target. sama classnya atau id nya 
+  oh ya gk usah di urutkan langsung aja pada target. gk usah di urutkan komponen1 2 3 4 dll saat select. langsung saja.
   
-  oh ya gk usah di urutkan langsung aja pada target. gk usah di urutkan komponen1 2 3 4 dll. langsung saja.
-  
-  
-  
-  oh iya, javascript itu all is object , all is array. banyak main data lebih. 1variable '',2array ['',''],3object {satu:'',dua:''}
+  oh iya, javascript itu all is object , all is array. banyak manipulasi object dan array. 1variable '',2array ['',''],3object {satu:'',dua:''}
   
   promise menggantikan if else yg banyak bisa antrian juga, async await untuk task schedule pada kondisi
-  
-  
-  
-  
-  ======= reference belajar javascript:
-  
-  
-  https://www.udemy.com/user/bina-nusantara-university/?key=taught_courses&taught_courses=4
-  
-  https://javascript.info/
-  
-  https://addyosmani.com/resources/essentialjsdesignpatterns/book/
-  
-  https://eloquentjavascript.net/
-  
-  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_Types
-  
-  
-  https://devhints.io/
-  http://overapi.com/express
-  https://github.com/azat-co/cheatsheets/tree/master/express4
-  https://medium.freecodecamp.org/modern-frontend-hacking-cheatsheets-df9c2566c72a
-  https://github.com/uanders/react-redux-cheatsheet
   
   **/
   
@@ -148,3 +122,30 @@ var diskon = (cape) => {
 
   
 module.exports = {nime,recom,free,diskon};
+
+
+
+
+
+/**
+  ======= reference belajar javascript:
+  
+  
+  https://www.udemy.com/user/bina-nusantara-university/?key=taught_courses&taught_courses=4
+  
+  https://javascript.info/
+  
+  https://addyosmani.com/resources/essentialjsdesignpatterns/book/
+  
+  https://eloquentjavascript.net/
+  
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_Types
+  
+  
+  https://devhints.io/
+  http://overapi.com/express
+  https://github.com/azat-co/cheatsheets/tree/master/express4
+  https://medium.freecodecamp.org/modern-frontend-hacking-cheatsheets-df9c2566c72a
+  https://github.com/uanders/react-redux-cheatsheet
+  
+**/
